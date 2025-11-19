a28cee34a8d72ca36f925783871269042f6021c7
BimaSandi19 <118245473+BimaSandi19@users.noreply.github.com>
2025-11-19 21:04:43 +0700

    Add pagination configuration file with default Bootstrap styling

---
+++ config/pagination.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Pagination View
+    |--------------------------------------------------------------------------
+    |
+    | This option defines the view that will be used to render the pagination
+    | links on your pages. By default, it is set to use Bootstrap styling but
+    | you are free to switch to a different style of your choosing.
+    |
+    | Supported: "tailwind", "bootstrap-4", "bootstrap-5", "custom-bootstrap-5"
+    |
+    */
+
+    'view' => 'pagination.custom-bootstrap-5',
+
+];
